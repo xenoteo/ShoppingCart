ecba189ef3e54c7524508211f859f9bfee0b31ec
xenoteo <fedorovxa@gmail.com>
2022-01-08 21:42:38 +0100

    Tests updated

--- src/test/java/com/xgen/interview/ShoppingCartTest.java
+++ src/test/java/com/xgen/interview/ShoppingCartTest.java
@@ -1,7 +1,5 @@
 package com.xgen.interview;
 
-import com.xgen.interview.Pricer;
-import com.xgen.interview.ShoppingCart;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -13,50 +11,36 @@ import static org.junit.Assert.assertEquals;
 public class ShoppingCartTest {
 
     @Test
-    public void canAddAnItem() {
+    public void canAddItems() {
         ShoppingCart sc = new ShoppingCart(new Pricer());
 
         sc.addItem("apple", 1);
+        sc.addItem("banana", 2);
 
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
         sc.printReceipt();
-        assertEquals(String.format("apple - 1 - €1.00%n"), myOut.toString());
-    }
-
-    @Test
-    public void canAddMoreThanOneItem() {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
-
-        sc.addItem("apple", 2);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-        assertEquals(String.format("apple - 2 - €2.00%n"), myOut.toString());
-    }
-
-    @Test
-    public void canAddDifferentItems() {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
-
-        sc.addItem("apple", 2);
-        sc.addItem("banana", 1);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-
         String result = myOut.toString();
 
-        if (result.startsWith("apple")) {
-            assertEquals(String.format("apple - 2 - €2.00%nbanana - 1 - €2.00%n"), result);
-        } else {
-            assertEquals(String.format("banana - 1 - €2.00%napple - 2 - €2.00%n"), result);
-        }
+        assertEquals("apple - 1 - €1.00\nbanana - 2 - €4.00\nTotal: €5.00\n", result);
+    }
+
+    @Test
+    public void supportsPriceFirstFormat() {
+        ShoppingCart sc = new ShoppingCart(new Pricer());
+        sc.setPriceFirst(true);
+
+        sc.addItem("apple", 1);
+        sc.addItem("banana", 2);
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        sc.printReceipt();
+        String result = myOut.toString();
+
+        assertEquals("€1.00 - apple - 1\n€4.00 - banana - 2\nTotal: €5.00\n", result);
     }
 
     @Test
@@ -69,6 +53,7 @@ public class ShoppingCartTest {
         System.setOut(new PrintStream(myOut));
 
         sc.printReceipt();
-        assertEquals(String.format("crisps - 2 - €0.00%n"), myOut.toString());
+        assertEquals("crisps - 2 - €0.00\nTotal: €0.00\n", myOut.toString());
     }
+
 }
